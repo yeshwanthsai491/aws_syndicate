8e3026cf6e26e7514194cc199c1a6a78d1552ae9
yeshwanth sai <yeshwanthsai491@gmail.com>
2025-03-07 09:47:00 +0530

    message

--- task03/jsrc/main/java/com/task03/HelloWorld.java
+++ task03/jsrc/main/java/com/task03/HelloWorld.java
@@ -21,7 +21,7 @@ public class HelloWorld implements RequestHandler<Object, Map<String, Object>> {
 		System.out.println("Hello from lambda");
 		Map<String, Object> resultMap = new HashMap<String, Object>();
 		resultMap.put("statusCode", 200);
-		resultMap.put("body", "Hello from Lambda");
+		resultMap.put("message", "Hello from Lambda");
 		return resultMap;
 	}
 }
